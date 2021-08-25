9efc2d16c9950de0b178cc3930db846f10eb8ca3
Thayna <thaynavaladaresclebio@gmail.com>
2021-08-24 21:56:14 -0300

    até a aula 16

--- hdcevents/app/Http/Controllers/EventController.php
+++ hdcevents/app/Http/Controllers/EventController.php
@@ -27,4 +27,17 @@ class EventController extends Controller
     {
         return view('events.contact');
     }
+
+    public function store(Request $request)
+    {
+        $event = new Event;
+        $event->title = $request->title;
+        $event->city = $request->city;
+        $event->description = $request->description;
+        $event->private = $request->private;
+
+        $event->save();
+
+        return redirect('/')->with('msg', 'Evento criado com sucesso!');
+    }
 }
--- hdcevents/routes/web.php
+++ hdcevents/routes/web.php
@@ -16,5 +16,6 @@ use App\Http\Controllers\EventController;
 
 Route::get('/', [EventController::class, 'index']);
 Route::get('/events/create', [EventController::class, 'create']);
+Route::post('/events', [EventController::class, 'store']);
 Route::get('/events/product', [EventController::class, 'product']);
 Route::get('/events/contact', [EventController::class, 'contact']);
